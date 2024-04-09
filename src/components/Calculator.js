31650ba02cf83361bc99de56efd67b32541407e4
Trịnh Duy Hưng <57101685+HUNG-rushb@users.noreply.github.com>
2024-04-09 23:52:41 +0700

    test

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -134,7 +134,7 @@ function Calculator() {
     <>
       <div className="container">
         <div className="main">
-          <p>Calculator - WITH Pipeline</p>
+          <p>React Calculator - WITH Pipeline</p>
           <Display input={input} setInput={setInput} answer={answer} />
           <Buttons
             inputHandler={inputHandler}
